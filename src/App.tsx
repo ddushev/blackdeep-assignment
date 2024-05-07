983f28e81a28e1611ca4d4d797b78a7bf560d189
Daniel Dushev <danieldyshew@gmail.com>
2024-05-07 08:54:46 +0300

    App. add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import Form from "./components/ui/Form";
 
 function App() {
 
   return (
-    <Flex justifyContent="center" alignItems="center" h="100vh" my="50px">
-      <Form />
+    <Flex justifyContent="center" alignItems="center" h="100vh" bg="#F5CF1A">
+      <Box bg="white" p="50px" borderRadius="lg" boxShadow="5px 5px 5px 5px rgba(255, 50, 50, 0.2)">
+        <Form />
+      </Box>
     </Flex>
   )
 }
